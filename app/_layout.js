2f3906ffc58245f2c447801024aa027e28700a27
Julian Jacobs <jljacobs45@gmail.com>
2024-04-16 22:33:48 -0700

    websocket poc

--- app/_layout.js
+++ app/_layout.js
@@ -3,15 +3,16 @@ import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router/stack";
 import { useLocalSearchParams } from "expo-router";
 import { ModalPortal } from "react-native-modals";
+import { WebSocketProvider } from "../lib/contexts/WebSocketProvider";
 
 export default function Layout() {
   return (
-    <>
+    <WebSocketProvider>
       <StatusBar style="auto" />
       <Stack>
         <Stack.Screen name="index" options={{ headerTitle: "" }} />
       </Stack>
       <ModalPortal />
-    </>
+    </WebSocketProvider>
   );
 }
--- app/index.js
+++ app/index.js
@@ -1,11 +1,38 @@
 import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
 import { GlobalStyles } from "../lib/styles";
 import RoundBtn from "../lib/components/RoundBtn";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import AddAddressModal from "../lib/components/AddAddressModal";
+import { WebSocketContext } from "../lib/contexts/WebSocketProvider";
+
+const TEST = ["bc1q0sg9rdst255gtldsmcf8rk0764avqy2h2ksqs5"];
 
 export default function App() {
   const [adding, setAdding] = useState(false);
+  const [addresses, setAddresses] = useState(TEST);
+  const { sendMessage, lastMessage } = useContext(WebSocketContext);
+
+  useEffect(() => {
+    for (let addr of addresses) {
+      console.log(addr);
+      sendMessage(
+        // JSON.stringify({
+        //   op: "addr_sub",
+        //   addr,
+        // })
+        JSON.stringify({
+          op: "ping",
+        })
+      );
+    }
+  }, [addresses]);
+
+  useEffect(() => {
+    console.log("res: ", lastMessage);
+    if (lastMessage) {
+      console.log(typeof lastMessage, JSON.parse(lastMessage.data));
+    }
+  }, [lastMessage]);
 
   return (
     <View style={styles.ctr}>
